4a558131428fb1843b2f0d2c3c6d6189467eb3b3
Rama Krishna Mundru <ramakrishnamundru471@gmail.com>
2016-11-20 00:28:35 +0530

    added prefix and postfix options
    added opotions to prefix and potfix text. also removed repeated divison.

--- jquery.countupcircle.js
+++ jquery.countupcircle.js
@@ -15,17 +15,20 @@
 		var settings = $.extend({
 			duration: 5000, //ms
 			opacity_anim: false,
-			step_divider: 1
+			step_divider: 1,
+			prefix:"",
+      			postfix:""
 		}, options);
 
 		var toCount = parseInt(this.html());
 
 		var i 	 		 = 0;
-		var step 		 = settings.duration / (toCount / settings.step_divider);
-		var procent_step = 1/(toCount / settings.step_divider);
+		var step_no=toCount/settings.step_divider;
+		var step= settings.duration /step_no;
+		var procent_step = 1/step_no;
 		var displayNumber = function() {
 			i=i+settings.step_divider;
-			self.html(i);
+			self.html(settings.prefix+i+settings.postfix);
 			if (settings.opacity_anim){
 				self.css({'opacity':procent_step*i});
 			}
@@ -38,7 +41,7 @@
 		};
 		
 		var set_endpoint = function (){
-			self.html(toCount);
+			self.html(settings.prefix+toCount+settings.postfix);
 		}
 
 		displayNumber();
